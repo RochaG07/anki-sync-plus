ed7b456bd4857e779391bfc108f5c507125ce7ee
RochaG07 <guilhermerochapsilva@gmail.com>
2023-08-11 15:59:39 -0300

    Card creation

--- main.ts
+++ main.ts
@@ -1,26 +1,31 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import {Converter} from "showdown";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface AnkiObsidianIntegrationSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: AnkiObsidianIntegrationSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AnkiObsidianIntegrationPlugin extends Plugin {
+	settings: AnkiObsidianIntegrationSettings;
+	htmlConverter : Converter;
+
+	exclusionRegex: RegExp = /# \*\*((.|\n)*)|---((.|\n)*)---/g;
 
 	async onload() {
 		await this.loadSettings();
+		this.htmlConverter = new Converter();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', async () => {
+			await this.scanVault();
 		});
+
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
@@ -28,51 +33,10 @@ export default class MyPlugin extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -89,28 +53,124 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+
+	getFilesOnFolder(folder: string) : TFile[]{
+		const files = this.app.vault.getMarkdownFiles().filter(file => file.parent?.path == folder);
+
+		return files;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	getAnkiCardIdFromNote(note: string): number | null{
+		let m = note.match(/anki-id: \d+/g)?.toString();
+
+		if (!m) return null;
+
+		return Number(m.match(/\d+/))
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	extractYamlFromNote(note: string): string[]{	
+		let output:string[] = [];
+		let yaml = note.match(/---((.|\n)*)---/g)?.toString();
+
+		if(yaml){
+			output = [...yaml.replace(/---\n|\n---/g, "").split("\n")];
+		}
+		
+		return output;
+	}
+
+	getDeckFromTags(tags: RegExpMatchArray[]): string{
+		let deck = tags[0][0].slice(1);
+		let captalizedLetter = deck.charAt(0).toUpperCase();
+		deck = captalizedLetter + deck.slice(1);
+
+		return deck;
+	}
+
+
+	async scanVault(){
+		const files = this.getFilesOnFolder("Target Folder");
+
+		for (let i = 0; i < files.length; i++) {	
+			let noteTitle = files[i].name.substring(0, files[i].name.length - 3);	
+			let noteContent = await this.app.vault.cachedRead(files[i])
+			let ankiId = this.getAnkiCardIdFromNote(noteContent);
+
+			if (!ankiId){
+				this.addNewCard(files[i], noteTitle, noteContent);
+			}
+		}
+	}
+
+	async addNewCard(note: TFile, noteTitle: string, noteContent: string ){
+		let tags = [...noteContent.matchAll(/#[a-zA-Z0-9À-ÿ]+/g)];
+
+		let deck = "Padrão";
+		if(tags.length > 0){
+			deck = this.getDeckFromTags(tags);
+		}
+		
+		let noteContentWithExclusion = noteContent.replace(this.exclusionRegex, "");
+		let ankiId = await this.sendCardToAnki(noteTitle, this.htmlConverter.makeHtml(noteContentWithExclusion), deck);
+
+		// Add id from created card on note
+		await this.app.vault.process(note, (data) => {
+			let yamlArr = this.extractYamlFromNote(noteContent)
+			let noteWithoutYaml = data.replace(/---((.|\n)*)---/g, "");
+
+			yamlArr.push("anki-id: " + ankiId);
+
+			let newData = `---\n${yamlArr.join("\n")}\n---${yamlArr.length == 1 ? "\n":""}${noteWithoutYaml}`
+
+
+
+			return newData
+		})
+
+
+		// TODO Add id from created card on database
+
+	}
+
+
+	async sendCardToAnki(front: string, back: string, deck: string): Promise<string | null> {
+		const url = "http://localhost:8765/";
+
+
+		const body = JSON.stringify({
+			action: "addNote",
+			version: 6,
+			params: {
+				"note": {
+					"deckName": deck,
+					"modelName": "Básico",
+					"fields": {
+						"Frente": front,
+						"Verso": back
+						}    
+					}
+				}
+		});
+
+		let response = await fetch(url, {
+			method: "post",
+			body
+		}).then((response) => {
+			return response.json();
+		}).catch((error) => {
+			console.log(error);
+			return null;
+		})
+
+		return response.result;
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: AnkiObsidianIntegrationPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AnkiObsidianIntegrationPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
