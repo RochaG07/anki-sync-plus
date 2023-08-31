8f653259245b6d07797dbf9db37be2772a1ebb3b
RochaG07 <guilhermerochapsilva@gmail.com>
2023-08-31 09:12:58 -0300

    Updated command icons and descriptions

--- main.ts
+++ main.ts
@@ -31,33 +31,29 @@ export default class AnkiObsidianIntegrationPlugin extends Plugin {
 
 		this.addCommand({
 			id: "scanCommand",
-			name: "Add/Update all notes on selected folder",
-			icon: "dice",
+			name: "Add/Update cards for all notes of selected folder on Anki",
+			icon: "layers",
 			callback: () => handleScanVault(this.app.vault, this.settings, this.createdDecks),
 		});
-		this.addRibbonIcon("dice", "Add/Update all notes on selected folder", () => handleScanVault(this.app.vault, this.settings, this.createdDecks));
+		this.addRibbonIcon("layers", "Add/Update cards for all notes of selected folder on Anki", () => handleScanVault(this.app.vault, this.settings, this.createdDecks));
 
 
 		this.addCommand({
 			id: "addUpdateSingleCardCommand",
-			name: "Add/Update card for current note",
-			icon: "dice",
+			name: "Add/Update card for current note on Anki",
+			icon: "copy-plus",
 			callback: () => handleAddOrUpdateSingleFile(this.app.vault, this.settings, this.createdDecks),
 		});
-		this.addRibbonIcon('dice', 'Add/Update current note', () => handleAddOrUpdateSingleFile(this.app.vault, this.settings, this.createdDecks));
+		this.addRibbonIcon('copy-plus', 'Add/Update card for current note on Anki', () => handleAddOrUpdateSingleFile(this.app.vault, this.settings, this.createdDecks));
 
 
 		this.addCommand({
 			id: "deleteSingleCardCommand",
-			name: "Delete card for current note",
-			icon: "dice",
+			name: "Delete card for current note on Anki",
+			icon: "copy-minus",
 			callback: () => handleDeleteSingleFile(this.app.vault),
 		});
-		this.addRibbonIcon('dice', 'Delete current note', () => handleDeleteSingleFile(this.app.vault));
-
-
-		this.addRibbonIcon('dice', 'test', () => {
-		});
+		this.addRibbonIcon('copy-minus', 'Delete card for  current note on Anki', () => handleDeleteSingleFile(this.app.vault));
 
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
